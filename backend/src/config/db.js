b7f10d54d8b573cb456c3baf4b5669af8e2a813e
wizenn <minhqui2003tg@gmail.com>
2025-06-23 15:58:15 +0700

    update BE

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,7 @@ const Connection = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useUnifiedTopology: true,
         });
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
